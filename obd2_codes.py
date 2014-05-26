595eb553ac693da0c03d5f34f30d2dcc524ff2df
nuit <jan.hoersch@tmt.de>
2014-05-26 02:58:10 +0200

    fix trouble code lookup

--- obd2_codes.py
+++ obd2_codes.py
@@ -1788,8 +1788,11 @@ pcodes = {
 	"P3494": "Cylinder 12 Exhaust Valve Control Performance",
 	"P3495": "Cylinder 12 Exhaust Valve Control Circuit Low",
 	"P3496": "Cylinder 12 Exhaust Valve Control Circuit High",
-	"P3497": "Cylinder Deactivation System",
-        "U0001" : "High Speed CAN Communication Bus" , 
+	"P3497": "Cylinder Deactivation System"
+}
+
+ucodes = {
+	"U0001" : "High Speed CAN Communication Bus" , 
 	"U0002" : "High Speed CAN Communication Bus (Performance)" , 
 	"U0003" : "High Speed CAN Communication Bus (Open)" , 
 	"U0004" : "High Speed CAN Communication Bus (Low)" , 
@@ -2088,7 +2091,6 @@ pcodes = {
 	"U0429" : "Invalid Data Received From Steering Column Control Module" , 
 	"U0430" : "Invalid Data Received From Tire Pressure Monitor Module" , 
 	"U0431" : "Invalid Data Received From Body Control Module 'A'" 
-
 }
 
 pcode_classes = {
@@ -2113,8 +2115,17 @@ pcode_classes = {
     "P17XX": "Transmission",
     "P18XX": "Transmission",
     "P19XX": "Transmission",
-    
-    }
+    "P20XX": "",
+    "P21XX": "",
+    "P22XX": "",
+    "P23XX": "",
+    "P24XX": "",
+    "P25XX": "",
+    "P26XX": "",
+    "P27XX": "",
+    "P2AXX": "O2 Sensors",
+    "P34XX": "Cylinder"
+}
 
 ptest= [
       "DTCs:",
